3e97278914abc5af23618f7360c63b510cd0de15
yogarevy <yogarevysetiadi@gmail.com>
2021-02-15 16:41:38 +0700

    Initial Commit

---
+++ yogarevy/providers/MapperServiceProvider.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class MapperServiceProvider extends ServiceProvider {
+  /**
+   * Register namespaces to the IoC container
+   *
+   * @method register
+   *
+   * @return {void}
+   */
+  register () {
+    //
+    this.app.singleton('Mapper', () => {
+      const MapperService = require('../src/MapperService')
+      return new MapperService()
+    })
+  }
+
+  /**
+   * Attach context getter when all providers have
+   * been registered
+   *
+   * @method boot
+   *
+   * @return {void}
+   */
+  boot () {
+    //
+  }
+}
+
+module.exports = MapperServiceProvider
